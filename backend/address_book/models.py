330419ae8b6f4aaebf7547e5c9b625c239313530
BMWdejf <sablatura.david@gmail.com>
2024-03-31 12:23:46 +0200

    Writing module database structure for importing data from Flexibee API

--- backend/address_book/models.py
+++ backend/address_book/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Address(models.Model):
+    fb_id = models.IntegerField()
+    last_update = models.DateTimeField()
+    kod = models.CharField()
+    nazev = models.TextField()
+    ic = models.IntegerField(max_length=11)
+    dic = models.CharField(max_length=13)
+    ulice = models.CharField()
+    mesto = models.CharField()
+    psc = models.CharField()
+
+    def __str__(self):
+        return self.nazev
